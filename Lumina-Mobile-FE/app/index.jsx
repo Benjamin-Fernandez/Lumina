5d2e3b44dcfc61ea4c288779fe9d397de6ccd1df
yanchi <yanchi97@gmail.com>
2024-10-06 22:25:44 +0800

    authentication ok

--- Lumina-Mobile-FE/app/index.jsx
+++ Lumina-Mobile-FE/app/index.jsx
@@ -83,6 +83,7 @@ export default function App() {
                       },
                       discovery
                     ).then((res) => {
+                      console.log(res);
                       setToken(res.accessToken);
                       router.push("/home");
                     });
